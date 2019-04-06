b9bde885222606674093a41d5c448e1c2091bc3f
ssaleem <sara@sarasaleem.com>
2019-04-06 14:48:09 -0400

    Fix comments

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -40,7 +40,7 @@ public class JobData {
                 values.add(aValue);
             }
         }
-//        sort results alphabetically
+//        sort results alphabetically (A-Z comes before a-z)
         Collections.sort(values);
 
         return values;
@@ -62,13 +62,15 @@ public class JobData {
      * with "Enterprise Holdings, Inc".
      *
      * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param value Value of the field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
 
         // load data, if not already loaded
         loadData();
+
+        // case-insensitive matching
         value = value.toLowerCase();
 
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
